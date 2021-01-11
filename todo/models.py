68100928ad9531e26c3ddb50f68612c7739757d1
Karl Xiao <kzx@andrew.cmu.edu>
2021-01-11 14:03:42 -0500

    made description optional

--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
   title = models.CharField(max_length=120)
-  description = models.TextField()
+  description = models.TextField(blank=True)
   date_added = models.DateField(null=True)
   completed = models.BooleanField(default=False)
 
